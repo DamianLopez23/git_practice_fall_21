d23a9b7fb02c7f1d61f092246a6b1ea51cc04a17
Damian Lopez <elilopez10@live.com>
2021-08-24 20:15:55 -0600

    init

---
+++ feature.rb
@@ -0,0 +1,7 @@
+# logging app
+# say_hello say_age
+
+def say_hello(name)
+  puts "hello #{name}"
+end
+say_hello('asdf')
---
+++ main.rb
@@ -0,0 +1,14 @@
+
+
+def add(num1,num2)
+num1 + num2
+end
+
+puts add(1,1)
+
+
+
+def minus(num1, num2)
+num1 -num2
+end
+puts minus(1,1)
